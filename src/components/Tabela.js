b4c4d23942b0e924e24df5d99fd464c453fbc583
Vinícius Reis <vinicius.reis@aevo.com.br>
2020-03-13 10:46:15 -0300

    Componente de Tabela criado

---
+++ src/components/Tabela.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+const TableHead = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Autores</th>
+        <th>Livros</th>
+        <th>Preços</th>
+        <th>Remover</th>
+      </tr>
+    </thead>
+  );
+};
+
+const TableBody = ({ content }) => {
+  return (
+    <tbody>
+      {content.map(({ nome, livro, preco }) => (
+        <tr>
+          <td>{nome}</td>
+          <td>{livro}</td>
+          <td>{preco}</td>
+          <td>
+            <button>Remover</button>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  );
+};
+
+export default class Tabela extends Component {
+  render() {
+    const { content } = this.props; //propriedade implicida que armazena as propriedades co componente
+
+    return (
+      <table>
+        <TableHead />
+        <TableBody content={content} />
+      </table>
+    );
+  }
+}

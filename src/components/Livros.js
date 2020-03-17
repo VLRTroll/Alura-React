10477f8e5177b88c4f0f1788e29feb1c38687d5f
ViniciusL <vinilucasreis1999@yahoo.com.br>
2020-03-17 17:43:55 -0300

    Páginas de Autores e Livros Preenchidas

--- src/components/Livros.js
+++ src/components/Livros.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import Header from './Header';
+import DataTable from './DataTable';
 
 // import { Container } from './styles';
 
 export default function Livros() {
+	const autores = [
+		{
+			nome: 'Paulo',
+			livro: 'React',
+			preco: '1002'
+		},
+		{
+			nome: 'Daniel',
+			livro: 'Java',
+			preco: '99'
+		},
+		{
+			nome: 'Marcos',
+			livro: 'Design',
+			preco: '150'
+		},
+		{
+			nome: 'Bruno',
+			livro: 'DevOps',
+			preco: '100'
+		},
+		{
+			nome: 'Vinicius',
+			livro: 'React',
+			preco: '500'
+		}
+	];
+
 	return (
 		<>
 			<Header />
 			<h1>Livros</h1>
+			<DataTable columnOrder={['livro']} content={autores} />
 		</>
 	);
 }

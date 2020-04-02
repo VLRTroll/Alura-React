e1069bdceed8d3d981d71e70fc13fa998d774a6e
VLRTroll <viniciusldr99@gamil.com>
2020-04-02 11:22:52 -0300

    Conteudo e estilização adicionados à página Sobre

--- src/pages/Sobre/Sobre.js
+++ src/pages/Sobre/Sobre.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import Header from '../../components/Header';
-
-// import { Container } from './styles';
+import React from "react";
+import Header from "../../components/Header";
+import { Typography, Container } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 export default function Sobre() {
-	return (
-		<>
-			<Header />
-			<h1>Sobre</h1>
-		</>
-	);
+  const styles = useStyles();
+
+  return (
+    <>
+      <Header />
+
+      <Container maxWidth="md">
+        <Typography component="h2" variant="h1" className={styles.title}>
+          Sobre
+        </Typography>
+
+        <Typography component="p" variant="body1">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel quia
+          quaerat laboriosam assumenda. Ipsam sed id earum harum ratione
+          repellat itaque dolorem fugiat voluptate! Perferendis, necessitatibus?
+          Autem odit aspernatur quo?
+        </Typography>
+      </Container>
+    </>
+  );
 }
+
+const useStyles = makeStyles({
+  title: {
+    textAlign: "center",
+    color: "black"
+  }
+});

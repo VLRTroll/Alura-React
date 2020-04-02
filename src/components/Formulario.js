f1af445c0e26fcd13553d915dcbc76ed8a1787e5
VLRTroll <viniciusldr99@gamil.com>
2020-04-02 12:38:54 -0300

    Componente Formulário refatorado

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,77 +1,74 @@
-import React, { useRef } from 'react';
-import Validator from '../validations/Validator';
-import FormConstraints from '../validations/FormValidator';
+import React, { useRef } from "react";
+import Validator from "../validations/Validator";
+import FormConstraints from "../validations/FormValidator";
+import { TextField, Grid, Button } from "@material-ui/core";
 
 export default function Formulario({ onsubmit }) {
-	const input_nome = useRef('');
-	const input_livro = useRef('');
-	const input_preco = useRef('');
+  const input_nome = useRef("");
+  const input_livro = useRef("");
+  const input_preco = useRef("");
 
-	const resetForm = () => {
-		input_nome.current.value = '';
-		input_livro.current.value = '';
-		input_preco.current.value = '';
-		input_nome.current.focus();
-	};
+  const resetForm = () => {
+    input_nome.current.value = "";
+    input_livro.current.value = "";
+    input_preco.current.value = "";
+    input_nome.current.focus();
+  };
 
-	const handleSubmit = event => {
-		event.preventDefault();
+  const handleSubmit = event => {
+    event.preventDefault();
 
-		const state = {
-			nome: input_nome.current.value,
-			livro: input_livro.current.value,
-			preco: input_preco.current.value
-		};
+    const state = {
+      nome: input_nome.current.value,
+      livro: input_livro.current.value,
+      preco: input_preco.current.value
+    };
 
-		if (Validator.isValid(state, FormConstraints)) {
-			onsubmit(state);
-			resetForm();
-		}
-	};
+    if (Validator.isValid(state, FormConstraints)) {
+      onsubmit(state);
+      resetForm();
+    }
+  };
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<div className='row'>
-				<div className='input-field col s4'>
-					<label className='input-field' htmlFor='nome'>
-						Nome
-					</label>
-					<input
-						ref={input_nome}
-						type='text'
-						name='nome'
-						className='validate'
-					/>
-				</div>
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item>
+            <TextField
+              inputRef={input_nome}
+              id="nome"
+              label="Nome"
+              variant="outlined"
+            />
+          </Grid>
 
-				<div className='input-field col s4'>
-					<label className='input-field' htmlFor='livro'>
-						Livro
-					</label>
-					<input
-						ref={input_livro}
-						type='text'
-						name='livro'
-						className='validate'
-					/>
-				</div>
+          <Grid item>
+            <TextField
+              inputRef={input_livro}
+              id="livro"
+              label="Livro"
+              variant="outlined"
+            />
+          </Grid>
 
-				<div className='input-field col s4'>
-					<label className='input-field' htmlFor='preco'>
-						Preço
-					</label>
-					<input
-						ref={input_preco}
-						type='number'
-						name='preco'
-						className='validate'
-					/>
-				</div>
-			</div>
+          <Grid item>
+            <TextField
+              inputRef={input_preco}
+              id="preco"
+              label="Preço"
+              variant="outlined"
+              type="number"
+            />
+          </Grid>
 
-			<button className='waves-effect waves-light btn blue lighten-2'>
-				Salvar
-			</button>
-		</form>
-	);
+          <Grid item>
+            <Button variant="contained" color="primary" type="submit">
+              Salvar
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </>
+  );
 }

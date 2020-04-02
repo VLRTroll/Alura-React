8080faff1fb1d5f56f29c8cf0d4bc6f56e97b838
VLRTroll <viniciusldr99@gamil.com>
2020-04-02 12:58:43 -0300

    ApiService refatorado

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,37 +1,33 @@
+const URL_BASE = "http://localhost:8000/api/autor";
+
 const checkError = response => {
-	if (response.ok) return response.json();
-	throw new Error(response.responseText);
+  if (response.ok) return response;
+  throw new Error(response.responseText);
+};
+
+const consumeApi = (endpoint = "", method = "GET", params = {}) => {
+  return fetch(`${URL_BASE}/${endpoint}`, { method, ...params })
+    .then(async response => checkError(response))
+    .then(response => response.json())
+    .then(json => json.data);
 };
 
 const ApiService = {
-	ListaAutores: () =>
-		fetch('http://localhost:8000/api/autor')
-			.then(async response => checkError(response))
-			.then(json => json.data),
+  ListaAutores: () => consumeApi(),
 
-	CriaAutor: autor =>
-		fetch('http://localhost:8000/api/autor', {
-			method: 'POST',
-			headers: { 'content-type': 'application/json' },
-			body: JSON.stringify(autor)
-		})
-			.then(async response => checkError(response))
-			.then(json => json.data),
+  CriaAutor: autor =>
+    consumeApi("", "POST", {
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(autor)
+    }),
 
-	RemoveAutor: id =>
-		fetch(`http://localhost:8000/api/autor/${id}`, {
-			method: 'DELETE',
-			headers: { 'Content-Type': 'application/json' }
-		}).then(async response => checkError(response)),
+  RemoveAutor: id =>
+    consumeApi(`${id}`, "DELETE", {
+      headers: { "Content-Type": "application/json" }
+    }),
 
-	ListaNomes: () =>
-		fetch('http://localhost:8000/api/autor/nome')
-			.then(async response => checkError(response))
-			.then(json => json.data),
+  ListaNomes: () => consumeApi("nome"),
 
-	ListaLivros: () =>
-		fetch('http://localhost:8000/api/autor/livro')
-			.then(async response => checkError(response))
-			.then(json => json.data)
+  ListaLivros: () => consumeApi("livro")
 };
 export default ApiService;
